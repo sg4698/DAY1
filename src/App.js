06e9849beb27d9d9d802d41c41a309fe537a41c2
Shivam Gupta <guptashivam8005@gmail.com>
2024-07-23 13:10:39 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import { Button , TextField} from '@mui/material';
+
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Registration form</h1>
+        <div className="abc">
+          <TextField
+            id="standard-basic"
+            label="First name"
+            variant="outlined"
+            placeholder="Enter your First number"
+            required
+          />
+        </div>
+        <div className="abc">
+          <TextField
+            id="standard-basic"
+            label="last name"
+            variant="outlined"
+            placeholder="Enter your last namr"
+            required
+          />
+        </div>
+
+        <div className="abc">
+          <TextField
+            id="standard-basic"
+            label="Email Id"
+            variant="outlined"
+            placeholder="Enter your Email Id"
+            required
+          />
+        </div>
+
+        <div className="abc">
+          <TextField
+            id="standard-basic"
+            label="Phone no"
+            variant="outlined"
+            placeholder="Enter your Phone no"
+            required
+          />
+        </div>
+        <div className="abc">
+          <TextField
+            id="standard-basic"
+            label="Password"
+            variant="outlined"
+            placeholder="Enter your Password"
+            required
+          />
+        </div>
+
+        <div className="abc">
+          <TextField
+            id="standard-basic"
+            label=" Confirm Password"
+            variant="outlined"
+            placeholder="Enter your  Confirm Password"
+            required
+          />
+        </div>
+      <div>
+      <Button type="submit" variant="contained" color="primary"  >Submit</Button>
+
+      </div>
       </header>
     </div>
   );
